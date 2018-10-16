5b763b69c1bdfb4aeb504f39d58c0587998be612
NedrNout <maxim.nedozrelov@ya.ru>
2018-10-16 23:03:27 +0300

    php 3 урок 4,5 задание решение препода

--- Lesson_3/Lesson_3_4,5.php
+++ Lesson_3/Lesson_3_4,5.php
@@ -1,18 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 13.10.2018
- * Time: 10:50
- */
+
 /*4. Объявить массив, индексами которого являются буквы русского языка,
 а значениями – соответствующие латинские буквосочетания
 (‘а’=> ’a’, ‘б’ => ‘b’, ‘в’ => ‘v’, ‘г’ => ‘g’, …, ‘э’ => ‘e’, ‘ю’ => ‘yu’, ‘я’ => ‘ya’).
 Написать функцию транслитерации строк.*/
+/*$arr = preg_split('//u', $word, -1, PREG_SPLIT_NO_EMPTY);
+    $a = 0;
+    foreach ($arr as $item) {
+        $arrray1 = [];
+        $arrray2 = [];
+        foreach ($alphabet as $abc => $kirillic) {
+            array_push($arrray1, $abc);
+            array_push($arrray2, $kirillic);
+            $key2 = array_search($item, $arrray1);
+            $arr[$a] = $arrray2[$key2];
+        }
+        $a++;
+    }
+    $str = implode('', $arr);*/
 
-transliterations("привет че как дела я хз как но это работает");
+transliterations("привет че как !! ыадвлпоfs k");
 
-function transliterations($word)
+function transliterations($str)
 {
     $alphabet = [
         'а' => 'a',
@@ -41,7 +50,7 @@ function transliterations($word)
         'ц' => 'c',
         'ч' => 'ch',
         'ш' => 'sh',
-        'щ' => 'shh',
+        'щ' => 'sch',
         'ъ' => '``',
         'ы' => 'y`',
         'ь' => '``',
@@ -50,21 +59,10 @@ function transliterations($word)
         'я' => 'ya',
         ' ' => '_',
     ];
-    $arr = preg_split('//u', $word, -1, PREG_SPLIT_NO_EMPTY);
-    $a = 0;
-    foreach ($arr as $item) {
-        $arrray1 = [];
-        $arrray2 = [];
-        foreach ($alphabet as $abc => $kirillic) {
-            array_push($arrray1, $abc);
-            array_push($arrray2, $kirillic);
-            $key2 = array_search($item, $arrray1);
-            $arr[$a] = $arrray2[$key2];
-        }
-        $a++;
-    }
-    $str = implode('', $arr);
-    echo $str;
+
+    $str = mb_strtolower($str);
+    $res = strtr($str, $alphabet);
+    echo $res;
 }
 
 
