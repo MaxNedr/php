a1190dba5dde5d62ca11e9bc0e4319b2a4773c56
NedrNout <maxim.nedozrelov@ya.ru>
2018-11-04 23:24:34 +0300

    php1 8 урок удаление и добавление товаров с проверкой на уникальность

--- Lesson_8/public_html/shop/category.php
+++ Lesson_8/public_html/shop/category.php
@@ -5,7 +5,8 @@ require '../../engine/core.php';
 /**
  * Выводим список категорий
  */
-function routeIndex() {
+function routeIndex()
+{
     $cats = getItemArray(
         "select * from category"
     );
@@ -18,7 +19,8 @@ function routeIndex() {
 /**
  * Выводим список товаров в категории
  */
-function routeView() {
+function routeView()
+{
     $id = $_GET['id'];
 
     $prods = getItemArray(
@@ -31,26 +33,29 @@ function routeView() {
 }
 
 
-if (isset($_POST['createNewCategory'])){
+if (isset($_POST['createNewCategory'])) {
     $name = $_POST['nameCategory'];
-    if (!empty($name)){
+    if (!empty($name)) {
         $sql = "Insert into `category`(category_name) value ('{$name}')";
         execute($sql);
-    }else{
+    } else {
 
         echo 'Пусто';
     }
 
-   header('Location: /shop/category.php');
+    header('Location: /shop/category.php');
 }
 
-function routeDeleteCategory(){
+function routeDeleteCategory()
+{
     $id = $_GET['id'];
     $sql = "delete from `category` where id={$id}";
     execute($sql);
 
 }
-function routeDeleteProduct(){
+
+function routeDeleteProduct()
+{
     $id = $_GET['id'];
     $sql = "delete from `product` where id={$id}";
     execute($sql);
@@ -101,41 +106,44 @@ function transliterations($str)
     return $res;
 }
 
-if (isset($_POST['createNewProduct'])){
-    $name = $_POST['nameProduct'];
-    $desc = $_POST['descriptionProduct'];
-    $price = $_POST['priceProduct'];
-    $quantity = $_POST['quantityProduct'];
-    $category_id = $_GET['id'];
-    $newFilename = $_SERVER['DOCUMENT_ROOT'];
-    $uploadInfo = $_FILES['img_file'];
-    switch ($uploadInfo['type']) {
-        case 'image/jpeg':
-            $newFilename1 ='/img/'.$name.'.jpg';
-            break;
+if (isset($_POST['createNewProduct'])) {
+    if (getItem("select * from `product` where name='{$_POST['nameProduct']}'") !== null) {
+        echo "есть в базе";
+    } else {
 
-        case 'image/png':
-            $newFilename1 = '/img/'.$name.'.png';
-            break;
+        $name = $_POST['nameProduct'];
+        $desc = $_POST['descriptionProduct'];
+        $price = $_POST['priceProduct'];
+        $quantity = $_POST['quantityProduct'];
+        $category_id = $_GET['id'];
+        $newFilename = $_SERVER['DOCUMENT_ROOT'];
+        $uploadInfo = $_FILES['img_file'];
+        switch ($uploadInfo['type']) {
+            case 'image/jpeg':
+                $newFilename1 = '/img/' . $name . '.jpg';
+                break;
 
-        default:
-            echo 'Файл неподдерживаемого типа';
-            exit;
-    }
-    $newFilename1 = transliterations($newFilename1);
+            case 'image/png':
+                $newFilename1 = '/img/' . $name . '.png';
+                break;
 
-    if (!move_uploaded_file($uploadInfo['tmp_name'], $newFilename.$newFilename1)) {
-        echo 'Не удалось осуществить сохранение файла';
-    }
+            default:
+                echo 'Файл неподдерживаемого типа';
+                exit;
+        }
+        $newFilename1 = transliterations($newFilename1);
 
-    $sql = "insert into `product`(name, description,price,quantity_in_stock, category_id,url_img) value 
+        if (!move_uploaded_file($uploadInfo['tmp_name'], $newFilename . $newFilename1)) {
+            echo 'Не удалось осуществить сохранение файла';
+        }
+
+        $sql = "insert into `product`(name, description,price,quantity_in_stock, category_id,url_img) value 
 ('{$name}','{$desc}','{$price}','{$quantity}','{$category_id}','{$newFilename1}')";
 
-    execute($sql);
-    header("Location: category.php?action=view&id={$_GET['id']}");
-
+        execute($sql);
+        header("Location: category.php?action=view&id={$_GET['id']}");
+    }
 }
-// Сделать проверку на существование URL и имени(оно должно быть уникальным)
 
 
 route();
